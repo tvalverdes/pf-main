af3f31aa1ff2e1d4a31205124b9e007c1eb4dc7c
Tyrone <tvalverdes9@gmail.com>
2023-12-30 11:48:45 -0500

    refactor: path rollback

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -3,12 +3,12 @@ import fs from 'fs'
 import matter from 'gray-matter'
 
 const getPostsMetadata = (): PostMetaData[] => {
-  const folder = 'app/posts/'
+  const folder = 'src/app/posts/'
   const files = fs.readdirSync(folder)
   const markdownPosts = files.filter((file) => file.endsWith('.md'))
 
   const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`app/posts/${fileName}`, 'utf8')
+    const fileContents = fs.readFileSync(`src/app/posts/${fileName}`, 'utf8')
     const matterResult = matter(fileContents)
     return {
       title: matterResult.data.title,

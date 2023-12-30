233584fdbde8bc5c4ec5c054e8c1c25de24895fa
Tyrone <tvalverdes9@gmail.com>
2023-12-30 13:10:44 -0500

    youtube component removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { Reputation } from '@/components/sections/home/reputation/Index'
 import { Statistics } from '@/components/sections/home/statistics/Index'
 import Testimonials from '@/components/sections/home/testimonials/Index'
 import { Topics } from '@/components/sections/home/topics/Index'
-
 export default function Home() {
+  
   return (
     <>
       <FinancialAdvice />

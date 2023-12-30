ac585de72b3cf201976b7848206a6981fa6bdfdc
Tyrone <tvalverdes9@gmail.com>
2023-12-30 13:44:20 -0500

    youtube component removed

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -3,12 +3,12 @@ import fs from 'fs'
 import matter from 'gray-matter'
 
 const getPostMetadata = (): PostMetaData[] => {
-  const folder = 'posts/'
+  const folder = 'src/posts/'
   const files = fs.readdirSync(folder)
   const markdownPosts = files.filter((file) => file.endsWith('.md'))
 
   const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, 'utf8')
+    const fileContents = fs.readFileSync(`src/posts/${fileName}`, 'utf8')
     const matterResult = matter(fileContents)
     return {
       title: matterResult.data.title,
